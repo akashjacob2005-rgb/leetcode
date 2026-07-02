21bed69d5ddc0eb945bf206993ed0ffba210867e
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-07-02 13:18:09 +0530

    Time: 0 ms (100%), Space: 22.6 MB (80.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,10 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int i=0;
         int n=nums.size();
-        for(int j=1;j<n; j++){
+
+        for(int j=1; j<n; j++){
             if(nums[i] != nums[j]){
-                nums[i+1]=nums[j];
-                i++;
+             nums[i+1] = nums[j];
+             i++;
             }
         }
         return i+1;
