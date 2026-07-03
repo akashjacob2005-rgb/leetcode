f599743b2fea4c5758af9c859bc9143833871073
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-07-03 22:58:29 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.34%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int xorr=0;
-        for(int i=0; i<nums.size(); i++){
+        int n= nums.size();
+        for(int i=0; i<n; i++){
             xorr = xorr^nums[i];
         }
         return xorr;
