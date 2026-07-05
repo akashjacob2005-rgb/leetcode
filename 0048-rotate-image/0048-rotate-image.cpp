22b82eeea9e3dba981887c0ef2db0afd6b9209ef
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-07-05 19:54:19 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
         int n=m.size();
-        for(int i=0; i<n-1;i++){
+
+        for(int i=0; i<n-1; i++){
             for(int j=i+1; j<n; j++){
                 swap(m[i][j], m[j][i]);
             }
@@ -11,6 +12,7 @@ public:
         for(int i=0; i<n; i++){
             reverse(m[i].begin(), m[i].end());
         }
+
         
     }
 };
