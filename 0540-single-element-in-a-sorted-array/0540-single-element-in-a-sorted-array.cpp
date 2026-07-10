037640214545de45944ae3cf66dc9803facd9a5f
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-07-10 18:12:48 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int low = 0, high = nums.size() - 1;
 
         while (low < high) {
-            int mid = low + (high - low) / 2;
+            int mid = (low + high) / 2;
 
             if (nums[mid] == nums[mid ^ 1])
                 low = mid + 1;
