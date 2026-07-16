07459f98b81d283d01618d801a7e9a34784071e5
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-07-16 22:53:38 +0530

    Time: 0 ms (100%), Space: 20.9 MB (50.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+
         int n=matrix.size();
         int m=matrix[0].size();
 
@@ -16,7 +17,7 @@ public:
             }
         }
 
-        for(int i=0; i<n; i++){
+        for(int i=0;i<n;i++){
             for(int j=0; j<m; j++){
                 if(row[i]==1 || col[j]==1){
                     matrix[i][j]=0;
