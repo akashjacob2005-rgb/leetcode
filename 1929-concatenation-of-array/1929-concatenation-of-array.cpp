c905507e2284aa31767ff07cdfe1338ca8cdca58
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-07-28 22:20:53 +0530

    Time: 0 ms (100%), Space: 16.6 MB (95.61%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -4,8 +4,9 @@ public:
         int n=nums.size();
         vector<int>ans(2*n);
 
-        for(int i=0; i<2*n; i++){
-            ans[i] = nums[i%n];
+        for(int i=0; i<n; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
         }
         return ans;
 
