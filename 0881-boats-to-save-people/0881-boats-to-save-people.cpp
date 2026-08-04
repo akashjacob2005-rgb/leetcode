9eb567e792505c4e90b5cfa1acc8a59490f509cf
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-08-04 15:40:00 +0530

    Time: 19 ms (47.98%), Space: 45.8 MB (71.89%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         int n=people.size();
-        int left=0; int right = n-1;
-        int count=0;
+        int left =0; int right =n-1;
+        int count =0;
+        
         sort(people.begin(), people.end());
 
         while(left<=right){
-            if(people[left]+people[right] <= limit){
+            if(people[left]+people[right]<= limit){
                 left++;
                 right--;
             }
@@ -17,6 +18,6 @@ public:
             count++;
         }
         return count;
-        
     }
+    
 };
