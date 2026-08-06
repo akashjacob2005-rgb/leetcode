eb94f48cdcc532343debe3f1f632b1761bbd9f49
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-08-06 16:24:34 +0530

    Time: 0 ms (100%), Space: 16.8 MB (70.35%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
 
-        int n = grid.size();
-        int m = grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
 
         queue<pair<int,int>> q;
-
         int fresh=0;
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
@@ -20,12 +20,11 @@ public:
         }
 
         int minutes=0;
-        int dx[4] = {-1,1,0,0};
-        int dy[4] = {0,0,-1,1};
+        int dx[4]={-1,1,0,0};
+        int dy[4]={0,0,-1,1};
 
         while(!q.empty() && fresh>0){
-            int size = q.size();
-
+            int size=q.size();
             for(int i=0; i<size; i++){
                 int x=q.front().first;
                 int y=q.front().second;
@@ -35,19 +34,16 @@ public:
                     int nx= x+dx[d];
                     int ny= y+dy[d];
 
-                    if(nx>=0 && nx<n &&
-                       ny>=0 && ny<m &&
+                    if(nx>=0 && nx<n && ny>=0 && ny<m &&
                        grid[nx][ny]==1){
+                       grid[nx][ny] =2;
+                       fresh--;
 
-                        grid[nx][ny]=2;
-                        fresh--;
-
-                        q.push({nx,ny});
+                       q.push({nx,ny});
                     }
-                    
+
                 }
             }
-
             minutes++;
         }
         if(fresh>0){
