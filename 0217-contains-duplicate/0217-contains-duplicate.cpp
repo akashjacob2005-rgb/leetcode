7b46b029b888372be49d57dc2e7105855f00bc21
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-08-24 16:52:57 +0530

    Time: 74 ms (45.56%), Space: 111.1 MB (70.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -5,9 +5,9 @@ public:
         unordered_set<int>seen;
 
         for(int i=0; i<nums.size(); i++){
-            if(seen.count(nums[i])==1)
-             return true;
-            
+            if(seen.count(nums[i])==1){
+                return true;
+            }
             seen.insert(nums[i]);
         }
         return false;
