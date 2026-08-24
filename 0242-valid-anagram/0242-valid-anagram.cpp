9e62920b2f6a430c4ec3bfcdac2937e6fdef2f84
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-08-24 17:10:02 +0530

    Time: 0 ms (100%), Space: 9.4 MB (99.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        if(s.size() != t.size())
-         return false;
-        
-        int count[26]={0};
+        if(s.size() != t.size()){
+            return false;
+        }
+        int count[26] = {0};
 
         for(int i=0; i<s.size(); i++){
             int index = s[i] - 'a';
