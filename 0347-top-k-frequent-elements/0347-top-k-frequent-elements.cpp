508ba2d661babe12f88fbe3c2fc1f950fa901dc7
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-08-31 10:41:55 +0530

    Time: 0 ms (100%), Space: 20.1 MB (9.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
-        unordered_map<int,int> count;
+        unordered_map<int,int>count;
+
         for(int i=0; i<nums.size(); i++){
             count[nums[i]]++;
         }
 
-        vector<vector<int>> bucket(nums.size()+1);
+        vector<vector<int>>bucket(nums.size()+1);
 
-        for(auto& pair : count){
+        for(auto& pair:count){
             int num = pair.first;
             int freq = pair.second;
 
             bucket[freq].push_back(num);
         }
-        
-
         vector<int>result;
 
         for(int i=bucket.size()-1; i>=0; i--){
@@ -28,6 +27,8 @@ public:
             }
         }
         return result;
+
+
         
     }
 };
