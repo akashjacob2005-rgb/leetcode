5caa0ee86adc705ee8ce49e0fe534379ae4bbcb0
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-09-06 23:40:10 +0530

    Time: 69 ms (25.49%), Space: 151.1 MB (80.27%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -6,29 +6,27 @@ public:
         
     }
     
-    void push(int val) {
-        st.push(val);
-        if(mini.empty())
-            mini.push(val);
-        else
-            mini.push(min(val,mini.top()));
-        
+    void push(int value) {
+        st.push(value);
+        if(mini.empty()){
+            mini.push(value);
+        }
+        else{
+            mini.push(min(value, mini.top()));
+        }
     }
     
     void pop() {
         st.pop();
         mini.pop();
-        
     }
     
     int top() {
         return st.top();
-        
     }
     
     int getMin() {
         return mini.top();
-        
     }
 };
 
