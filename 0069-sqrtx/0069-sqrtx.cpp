7ee03fa27ff45597cdf5537c7f26fa93e1ed564a
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-09-15 23:28:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.27%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x<2){
-            return x;
-        }
 
-        long long left = 1;
-        long long right = x/2;
+        if(x<2)
+         return x;
+        
+        int low=1;
+        int high=x;
+        int ans=0;
 
-        while(left<=right){
-            long long mid = left+(right-left)/2;
-
-            if(mid*mid == x){
-                return mid;
-            }
-            if(mid*mid <x){
-                left = mid+1;
+        while(low<=high){
+            long long mid = low+(high-low)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                low = mid+1;
             }
             else{
-                right = mid-1;
+                high = mid-1;
             }
-
         }
-        return right;
-        
+        return ans;
     }
 };
