a4019d31b41eba4167cc6e32cda41c70a1c12a68
akashjacob2005-rgb <akashjacob2005@gmail.com>
2026-09-24 19:17:21 +0530

    Time: 5 ms (78.64%), Space: 22.9 MB (58.44%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -5,22 +5,23 @@ public:
         int left = 1;
         int right = *max_element(piles.begin(), piles.end());
 
-        while(left<right){
+        while(left < right){
             int mid = left+(right-left)/2;
-            long long hours=0;
 
+            long long hours =0;
             for(int pile:piles){
-                hours += (pile+mid-1)/mid;
+                hours+= (pile+mid-1)/mid;
             }
 
             if(hours <= h){
-                right =mid;
+                right=mid;
             }
             else{
                 left=mid+1;
             }
         }
         return left;
+
         
     }
 };
